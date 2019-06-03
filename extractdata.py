e82bc169baf490192ce4cdc037b6d32183789a0f
sepideh <43911622+sepidehsayad@users.noreply.github.com>
2019-06-03 13:43:04 +0430

    Add files via upload

--- extractdata.py
+++ extractdata.py
@@ -5,8 +5,13 @@ secret_image = Image.open('new_img.bmp','r')
 
 pixel_value_secret_data = list(secret_image.getdata())
 #print(pixel_value_secret_data)
-k = input("input k")
+k = input("input k :")
+if 1<= int(k) <= 4 :
+    print("it okay,see scret data")
+else :
+    print("enter another k")
 
+    
 k_LSB = int(k)
 
 
@@ -29,9 +34,50 @@ def extract_data(binary_of_secret_image,k_LSB):
         list_of_data+=[binary_of_secret_image[i][-k_LSB:]]
 
     return list_of_data
+
+
+def one_string(list_of_data):
+    string =''
+    for i in list_of_data :
+        string+=i
+    #print(string)
+    return string
+
+def spilit_string(string,pix_8_):
+    list_k=[]
+    for i in range(0,len(string),pix_8_):
+        list_k+=[string[i:i+pix_8_]]
+
+    
+    return list_k
+    
+    
+
+def show_new_picture(dec_pixels):
+    img = Image.new('L', (512,256))
+    img.putdata(dec_pixels)
+    img.save("new__img.bmp")
+    img.show()
+
+def conver_bin_to_decimal(bin_pixles):
+    dec_pixels=[]
+    for i in bin_pixles:
+        dec_pixels+=[int(i,2)]
+    return dec_pixels
+
+
 list_of_data=[]
 list_of_data=extract_data(binary_of_secret_image,k_LSB)
-#print(list_of_data)
+string_of_pixels = one_string(list_of_data)
+#print("len",string_of_pixels)
+spili_string=[]
+spili_string=spilit_string(string_of_pixels,8)
+dec_pixels = conver_bin_to_decimal(spili_string)
+show_new_picture(dec_pixels)
+#print(dec_pixels[0:30])
+#print(spili_string[0:30])
+#print(list_of_data[0:30])
+#print(list_of_data[0:69125])
 
 
 
--- new_LSB.py
+++ new_LSB.py
@@ -1,17 +1,21 @@
 from PIL import Image
 
 host_image = Image.open('baboon512.bmp','r') #open image 
+secret_image = Image.open('boat256.bmp','r') #open image
+###########################################################
+#file = open('secretdata.txt','r') #file of scret data
+secret_image = Image.open('boat256.bmp','r') #open image
 
-file = open('secretdata.txt','r') #file of scret data
 
 
 pixel_value_hostimage = list(host_image.getdata()) #we have pixels in this list
 
 
+pix_val_secret=list(secret_image.getdata())
 
 width, height =host_image.size #size of pic
 
-
+width1, height1 =host_image.size #size of pic
 def pixel_value(list_of_values): #RGB(R,G,B) in gray scale = RGB(X,X,X) so one of them is enough
     pixel_val=[]
     for i in list_of_values:
@@ -31,8 +35,11 @@ def size_of_file(file):#size of secret data
 
 def cal_of_k(size_of_file):
     pixels=host_image.size[0]*host_image.size[1]
-    return int(pixels/size_of_file) #calculate K-LSB (number of pixeles/size_of_file)
-
+    k = int(size_of_file/pixels)
+    if 1<= k <= 4 :
+        print("k :",k)
+        return k #calculate K-LSB (number of pixeles/size_of_file)
+    return "PLZ ENTER ANOTHER FILE"
 
 
 def convert_pixelvalue_to_binary(pixel_val):
@@ -43,6 +50,7 @@ def convert_pixelvalue_to_binary(pixel_val):
     return binary_of_pixel_value
 
 
+    
 
 def list_of_secret_data_k(file):#all the scret data in of string 
         with open("secretdata.txt", "r") as ins:
@@ -88,18 +96,31 @@ def show_new_picture(dec_pixels):
     img.putdata(dec_pixels)
     img.save("new_img.bmp")
     img.show()
-
+#########################
+Pixel_val_secret =pixel_value(pix_val_secret)
+binary_pixel_values_secret=convert_pixelvalue_to_binary(Pixel_val_secret)
+print(binary_pixel_values_secret[0:30])
 pixel_val =[]
+f_sec_pix='' 
+for i in range(len(binary_pixel_values_secret)) :
+    f_sec_pix +=binary_pixel_values_secret[i]
+    
+#print(f_sec_pix)
+
+#######
 pixel_val = pixel_value(pixel_value_hostimage)
 print("pixel value host :\n",pixel_val[0:30])
-char =size_of_file(file)
-print(char)
+#char =size_of_file(file)
+#print(char)
 binary_pixel_values=[]
 binary_pixel_values=convert_pixelvalue_to_binary(pixel_val)
 print("\n pixel value host binary :\n",binary_pixel_values[0:30])
-string_scret_data= []
-string_scret_data=list_of_secret_data_k(file)
-k_lsb=cal_of_k(char)
+string_scret_data= f_sec_pix
+#print("leeen",len(string_scret_data))
+#string_scret_data=list_of_secret_data_k(file)
+
+k_lsb=cal_of_k(len(string_scret_data))
+print("k",k_lsb)
 k_slicee_k_slicee_scret_data =[]
 k_slicee_k_slicee_scret_data =spilit_value_with_k(string_scret_data,k_lsb)
 print("len:",len(k_slicee_k_slicee_scret_data))
